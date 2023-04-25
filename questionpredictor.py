37a769ce6c82768ac10a9703011af1cc0149e795
Ben Smith <benjaminsmith1981@googlemail.com>
2023-04-26 00:47:32 +0100

    recency calc for both papers

--- questionpredictor.py
+++ questionpredictor.py
@@ -1,56 +1,79 @@
 import csv
+import re
 from collections import defaultdict
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
-# Read the CSV data from a file
-filename = 'questions.csv'
 
-with open(filename, 'r') as file:
-    reader = csv.reader(file)
-    data = [row for row in reader]
 
-# Remove leading/trailing spaces from header names
-header, data = [item.strip() for item in data[0]], data[1:]
+# The probability calculation in the updated code considers both the frequency and recency of the questions. 
+# Here's a detailed explanation of the calculation:
 
-# Count the occurrences of each question type
-question_counts = defaultdict(int)
+# The resulting question_probabilities dictionary contains the probability of each question type, 
+# considering both their frequency and recency.
+def read_and_sort_csv(filename):
+    with open(filename, 'r') as file:
+        reader = csv.reader(file)
+        data = [row for row in reader]
 
-for row in data:
-    for idx, cell in enumerate(row[1:], start=1):
-        if cell:
-            question_counts[header[idx]] += 1
+    header, data = [item.strip() for item in data[0]], data[1:]
+    # current_year: The year we are making the predictions for (in this case, 2023).
+    # year: The year the question appeared in the data.
+    # recency_weight_factor: A value between 0 and 1 that controls the impact of recency on the probabilities. 
+    current_year = 2023
+    recency_weight_factor = 0.5
 
-# Display the frequency of each question type
-print("Frequency of questions:")
-for question, count in question_counts.items():
-    print(f"{question}: {count}")
+    # Frequency: The number of times a particular question type appears in the data.
+    # Recency: The number of years that have passed since the question type last appeared.
+    # To account for both factors, we introduce a recency_weight for each question in the data, which is calculated as:
+    # recency_weight = 1 + recency_weight_factor * (current_year - year)
+    question_counts = defaultdict(float) # The updated count for each question type is stored in the question_counts dictionary.
 
-# Create a DataFrame and set the "Year" column to numeric
-df = pd.DataFrame(data, columns=header)
-print(df)
-df["Year"] = pd.to_numeric(df["Year"])
+    # A higher value gives more weight to recency, while a lower value reduces its impact.
+    # The recency_weight is multiplied by the frequency of the question type for each row in the data, 
+    # effectively increasing the count of questions that have appeared more recently. 
+    for row in data:
+        year = int(re.match(r'\d+', row[0]).group())
+        recency_weight = 1 + recency_weight_factor * (current_year - year)
+        for idx, cell in enumerate(row[1:], start=1):
+            if cell:
+                question_counts[header[idx]] += recency_weight
 
-# This algorithm essentially predicts the questions for 2023 by considering the historical frequency of each question type and selecting questions randomly, 
-# but with a higher probability for more frequent question types. This approach assumes that the distribution of questions in the past is representative of 
-# the distribution of questions in the future.# Calculate the total number of questions by summing the values in question_counts. 
-# This is done using sum(question_counts.values()).
-total_questions = sum(question_counts.values())
+    # Once we have the weighted counts, we calculate the total count of all question types:
+    # total_questions = sum(question_counts.values())
+    total_questions = sum(question_counts.values())
 
-# Calculate the probability of each question occurring by dividing the count of each question by the total number of questions. 
-# This is done using a dictionary comprehension: {question: count / total_questions for question, count in question_counts.items()}. 
-# The resulting question_probabilities dictionary contains the probabilities for each question type.
-question_probabilities = {question: count / total_questions for question, count in question_counts.items()}
+    # Next, we calculate the probability of each question type by dividing the weighted count of that question type 
+    # by the total count:
+    # question_probabilities = {question: count / total_questions for question, count in question_counts.items()}
+    question_probabilities = {question: count / total_questions for question, count in question_counts.items()}
+    
+    return question_probabilities
 
-# Predict the possible questions for 2023
-# Calculate the average number of questions per year by taking the mean of the number of questions in each row, excluding the 'Year' column: 
-# int(np.mean([len(row) - 1 for row in data])). 
-# This gives an estimate of the number of questions to expect in 2023.
-num_questions_2023 = int(np.mean([len(row) - 1 for row in data]))
+csv1 = 'questions1.csv'
+csv2 = 'questions2.csv'
 
-# Use numpy's random.choice function to randomly select num_questions_2023 questions from the available question types, weighted by their probabilities. 
-# The p parameter in the np.random.choice function ensures that the selection is weighted by the probabilities calculated earlier.
-predicted_questions_2023 = np.random.choice(list(question_probabilities.keys()), size=num_questions_2023, p=list(question_probabilities.values()))
+probabilities1 = read_and_sort_csv(csv1)
+probabilities2 = read_and_sort_csv(csv2)
 
-print("\nPredicted questions for 2023:")
-print(predicted_questions_2023)
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(16, 6), sharey=True)
+
+truncate_labels = lambda x: (x[:22] + '...') if len(x) > 25 else x
+
+ax1.bar(probabilities1.keys(), probabilities1.values())
+ax1.set_xticks(range(len(probabilities1.keys())))
+ax1.set_xticklabels(map(truncate_labels, probabilities1.keys()), rotation=90, fontsize=8)
+ax1.set_xlabel('Question Types')
+ax1.set_ylabel('Probability')
+ax1.set_title('Probabilities from CSV1')
+
+ax2.bar(probabilities2.keys(), probabilities2.values())
+ax2.set_xticks(range(len(probabilities2.keys())))
+ax2.set_xticklabels(map(truncate_labels, probabilities2.keys()), rotation=90, fontsize=8)
+ax2.set_xlabel('Question Types')
+ax2.set_title('Probabilities from CSV2')
+
+plt.suptitle('Probabilities of Topics Appearing in 2023 (Calculated as Count of Each Topic / Total Count)')
+
+plt.show()
